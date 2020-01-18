2a81f106fa0ffb4a88c4302a8214a081441e348b
luiginicastro <58125435+luiginicastro@users.noreply.github.com>
2020-01-18 16:32:50 -0500

    Inital commit.

---
+++ Assets/Scripts/HandleGazeInput.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using A11YTK;
+using UnityEngine;
+
+public class HandleGazeInput : MonoBehaviour {
+    public Camera viewCamera;
+    public LayerMask hitMask;
+    [SerializeField] private Material highlightedMaterial;
+    [SerializeField] private Material defaultMaterial;
+
+    private Transform _selection;
+
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Ray ray = new Ray (viewCamera.transform.position, viewCamera.transform.rotation * Vector3.forward);
+        RaycastHit hit;
+
+        if (_selection != null) {
+            var selectionRenderer = _selection.GetComponent<Renderer> ();
+            selectionRenderer.material = defaultMaterial;
+            _selection = null;
+        }
+
+        if (Physics.Raycast (ray, out hit, Mathf.Infinity, hitMask)) {
+            Debug.Log ("I WORK");
+            var selection = hit.transform;
+            var selectionRenderer = selection.GetComponent<Renderer> ();
+            if (selectionRenderer != null) {
+                selectionRenderer.material = highlightedMaterial;
+            }
+
+            _selection = selection;
+
+            if (Input.GetKeyDown (KeyCode.Space)) {
+                var component = hit.collider.gameObject.GetComponent<SubtitleController> ();
+                if (component != null) {
+
+                    component.PlayOneShot ();
+                    Debug.Log ("I REALLY WORK");
+
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SimpleGazeScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using A11YTK;
+using UnityEngine;
+
+public class SimpleGazeScript : MonoBehaviour {
+    public Camera viewCamera;
+    public GameObject cursorPrefab;
+    public float maxCursorDistance = 30;
+
+    private GameObject cursorInstance;
+
+    // Start is called before the first frame update
+    void Start () {
+        cursorInstance = Instantiate (cursorPrefab);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        UpdateCursor ();
+    }
+
+    private void UpdateCursor () {
+        // Create a gaze ray pointing forward from the camera
+        Ray ray = new Ray (viewCamera.transform.position, viewCamera.transform.rotation * Vector3.forward);
+        RaycastHit hit;
+        if (Physics.Raycast (ray, out hit, Mathf.Infinity)) {
+            Debug.Log ("I WORK");
+            if (Input.GetKeyDown (KeyCode.Space)) {
+                var component = hit.collider.gameObject.GetComponent<SubtitleController> ();
+                if (component != null) {
+
+                    component.PlayOneShot ();
+                    Debug.Log ("I REALLY WORK");
+
+                }
+            }
+            // If the ray hits something, set the position to the hit point
+            // and rotate based on the normal vector of the hit
+            cursorInstance.transform.position = hit.point;
+            cursorInstance.transform.rotation = Quaternion.FromToRotation (Vector3.up, hit.normal);
+
+        } else {
+            // If the ray doesn't hit anything, set the position to the maxCursorDistance
+            // and rotate to point away from the camera
+            cursorInstance.transform.position = ray.origin + ray.direction.normalized * maxCursorDistance;
+            cursorInstance.transform.rotation = Quaternion.FromToRotation (Vector3.up, -ray.direction);
+        }
+    }
+}
