fbcbf514b6fce5a3b5f49060d7e1269a11b156d2
Scott Doxey <neogeek0101@gmail.com>
2020-01-19 10:04:13 -0500

    Added dev tools for enabling and disabling objects.

---
+++ Assets/DevTools.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class DevTools : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _controllers;
+    [SerializeField] private Behaviour[] _editorGazeComponets;
+
+    private void Start()
+    {
+        if (Application.isEditor)
+        {
+            foreach (var controller in _controllers)
+            {
+                controller.SetActive(false);
+            }
+        }
+        else
+        {
+            foreach (var component in _editorGazeComponets)
+            {
+                component.enabled = false;
+            }
+        }
+    }
+}
