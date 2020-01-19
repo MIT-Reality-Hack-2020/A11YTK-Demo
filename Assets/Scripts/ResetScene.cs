b159701bc6cf4c1234951e9a08b49eb8294b0910
luiginicastro <58125435+luiginicastro@users.noreply.github.com>
2020-01-19 12:18:13 -0500

    Text changes and the start of audio manager

---
+++ Assets/Scripts/ResetScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //So you can use SceneManager
+public class ResetScene : MonoBehaviour {
+    // Start is called before the first frame updatefunction Start () {
+
+    private void Update () {
+        if (Input.GetKeyDown ("r")) { //If you press R
+            SceneManager.LoadScene ("A11YTK"); //Load scene called Game
+
+        }
+    }
+
+}
---
+++ Assets/VolumeControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Video;
+
+public class VolumeControl : MonoBehaviour {
+    private UnityEngine.Video.VideoPlayer[] videoPlayers;
+
+    public GameObject[] videoPlayerContiner;
+
+    // Start is called before the first frame update
+    void Start () {
+        foreach (GameObject container in videoPlayerContiner) {
+            var player = container.transform.FindChild ("")
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
